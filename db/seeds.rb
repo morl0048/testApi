1bbaca715fc8ab92ae637efbdd24d95e4ade72e8
morl0048 <morneaulouka@hotmail.com>
2018-04-05 14:51:39 -0400

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Fournisseur.create(nom: Faker::Company.name, adresse: "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.country}",
+                     tel: Faker::PhoneNumber.phone_number)
+end
+
+5.times do |i|
+  Produit.create(nom: Faker::Lorem.word, quantite: Faker::Number.number(2), description: Faker::Lorem.sentence,
+  prix: Faker::Number.decimal(2, 3), fournisseur_id: i)
+end
